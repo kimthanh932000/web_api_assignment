11f5d5c7c4f539c302f7a85084156ad2f98ef4f0
VEE\LENOVO <kimthanh932000@gmail.com>
2025-05-21 13:04:28 +0800

    Add api response wrapper

---
+++ Backend/Backend/ApiResponse.cs
@@ -0,0 +1,31 @@
+﻿namespace API
+{
+    public class ApiResponse<T>
+    {
+        public string Message { get; set; }
+        public T? Data { get; set; }
+        public List<string>? Errors { get; set; }
+
+        public ApiResponse(T data, string message)
+        {
+            Message = message;
+            Data = data;
+        }
+
+        public ApiResponse(List<string> errors, string message)
+        {
+            Message = message;
+            Errors = errors;
+        }
+
+        public static ApiResponse<T> SuccessResponse(T data, string message)
+        {
+            return new ApiResponse<T>(data, message);
+        }
+
+        public static ApiResponse<T> FailedResponse(List<string> errors, string message = "An error has occurred")
+        {
+            return new ApiResponse<T>(errors, message);
+        }
+    }
+}
